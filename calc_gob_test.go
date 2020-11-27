0db8d2576dc3797f3ce395ccf32bfcd7236bf896
Adam Oon <adamoon91@gmail.com>
2020-11-27 12:35:11 +0800

    Update calc_gob_test.go
    add new describe block

--- calc_gob_test.go
+++ calc_gob_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestAdd(t *testing.T) {
 	gob := Goblin(t)
-	gob.Describe("Calculator Operations", func() {
+	gob.Describe("Calculator Add Operations", func() {
 		gob.It("it should sum up two integers", func() {
 			gob.Assert(Add(1, 2)).Equal(3)
 			gob.Assert(Add(4, 150)).Equal(154)
@@ -17,11 +17,13 @@ func TestAdd(t *testing.T) {
 			gob.Assert(Add(-5, 2)).Equal(-3)
 		})
 
+		
+	})
+	gob.Describe("Calculator Division Operations", func() {
 		gob.It("it should divide two integers", func() {
 			gob.Assert(Divide(1, 0)).Equal(math.Inf(1))
 			gob.Assert(Divide(6, 2)).Equal(3.0)
 			gob.Assert(Divide(12, 5)).Equal(2.4)
-
 		})
 	})
 }
